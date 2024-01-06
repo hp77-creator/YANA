4a0690e2d0b6b32e4ab855d98f6e2ff8548ed908
Himanshu Pandey <himanshu.pandey@piramal.com>
2024-01-06 10:09:59 +0530

    hp77 | Create skeleton for spring boot project

---
+++ src/main/java/com/hp77creator/YANA/YanaApplication.java
@@ -0,0 +1,13 @@
+package com.hp77creator.YANA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanaApplication.class, args);
+	}
+
+}
